191ccd11fd08db6353542bd8e341720c9bd60529
saarthak-guptaa <Saarthak22305@gmail.com>
2025-08-25 00:08:27 +0530

    Correcting

--- src/main/java/com/study/saarthak/VehicleTest.java
+++ src/main/java/com/study/saarthak/VehicleTest.java
@@ -2,7 +2,7 @@ package com.study.saarthak;
 
 public class VehicleTest  {
     public static void main(String[] args) {
-        Vehicleitf v1 = new Vehicle();
+        Vehicle v1 = new Vehicle();
         System.out.println(v1.getAcceleration());
         System.out.println(v1.getMaxSpeed());
         System.out.println(v1.offRoadingCapacity());
